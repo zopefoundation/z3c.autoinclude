5af5e7d4d9d0f01eaca360b2168dbbb1bade31c7
Ethan Jucovy <ethan.jucovy@gmail.com>
2008-12-07 00:41:51 +0000

    various code cleanup

--- src/z3c/autoinclude/tests/tests.py
+++ src/z3c/autoinclude/tests/tests.py
@@ -1,11 +1,7 @@
-import doctest
-import unittest
+from zc.buildout import testing
 
 import os
-from zc.buildout import testing
-from pprint import pprint
 projects_dir = os.path.dirname(__file__)
-from pkg_resources import working_set
 
 
 # this is the list of test packages that we'll temporarily install
@@ -16,9 +12,9 @@ test_packages = ['APackage', 'BCPackage', 'XYZPackage',
                  'base2', 'base2_plug', 'TestDirective']
 
 
+from zc.buildout.easy_install import install
+from pkg_resources import working_set
 def install_projects(projects, target_dir):
-    from zc.buildout.easy_install import install
-
     links = []
     for project in projects:
         project_dir = os.path.join(projects_dir, project)
@@ -29,44 +25,49 @@ def install_projects(projects, target_dir):
                 os.path.join('bin', 'buildout'), project_dir))
         links.append(dist_dir)
 
-    return install(projects, target_dir, links=links,
-                   working_set=working_set)
-
-
-def testSetUp(test):
-
-    testing.buildoutSetUp(test)
-        
-    import tempfile
-    target_dir = tempfile.mkdtemp('.z3c.autoinclude.test-installs')
-    
-    ws = install_projects(test_packages, target_dir)
+    new_working_set = install(projects, target_dir, links=links,
+                              working_set=working_set)
 
     # we must perform a magical incantation on each distribution
-    for dist in ws:
+    for dist in new_working_set:
         dist.activate()
-        
-        if dist.project_name == 'APackage':
-            test.globs['a_dist'] = dist
-        elif dist.project_name == 'XYZPackage':
-            test.globs['xyz_dist'] = dist
-        elif dist.project_name == 'SiblingPackage':
-            test.globs['sibling_dist'] = dist
-        elif dist.project_name == 'FooPackage':
-            test.globs['foo_dist'] = dist
-        elif dist.project_name == 'BasePackage':
-            test.globs['base_dist'] = dist
+    return new_working_set
+
+def testSetUp(test):
+    """
+    install test packages so that they can be imported
+    and their egg info examined in test runs
+    """
+    
+    testing.buildoutSetUp(test)
+
+    import tempfile
+    target_dir = tempfile.mkdtemp('.z3c.autoinclude.test-installs')    
+    install_projects(test_packages, target_dir)
 
 
+def testTearDown(test):
+    from testdirective.zcml import clear_test_log
+    clear_test_log()
+
+    testing.buildoutTearDown(test)
+
+
+import doctest
+import unittest
+    
 def test_suite():
-    suite = doctest.DocFileSuite('../utils.txt', '../dependency.txt', '../plugin.txt',
+
+    from pprint import pprint
+    suite = doctest.DocFileSuite('../utils.txt',
+                                 '../dependency.txt',
+                                 '../plugin.txt',
                                  setUp=testSetUp,
-                                 tearDown=testing.buildoutTearDown,
+                                 tearDown=testTearDown,
                                  globs={'pprint':pprint},
                                  optionflags=doctest.ELLIPSIS)
 
-    return unittest.TestSuite((suite,
-                               ))
+    return unittest.TestSuite((suite,))
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
