829a6fa5dcf65306ca9b19ebe43ff525fb3fce7b
Hanno Schlichting <hanno@hannosch.eu>
2009-12-19 18:12:27 +0000

    Prepare z3c.autoinclude 0.3.2.

--- setup.py
+++ setup.py
@@ -5,14 +5,12 @@ long_description = (open('README.txt').read()
                     open('CHANGES.txt').read())
 
 setup(name='z3c.autoinclude',
-      version='0.3dev',
+      version='0.3.2',
       description="Automatically include ZCML",
       long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Zope3",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
       author='Ethan Jucovy, Robert Marianski, Martijn Faassen',
@@ -27,11 +25,10 @@ setup(name='z3c.autoinclude',
       install_requires=[
           'setuptools',
           'zope.dottedname',
-	  'zope.interface',
-	  'zope.configuration',
-	  'zope.schema',
+          'zope.interface',
+          'zope.configuration',
+          'zope.schema',
           'zc.buildout',
-          # -*- Extra requirements: -*-
       ],
       extras_require={'test': ['zc.buildout','zope.testing']},
       entry_points="""
