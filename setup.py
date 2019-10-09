b96a6db35316525b0a73be8424ecc3cff1e66fd7
Maurits van Rees <maurits@vanrees.org>
2019-10-09 22:07:27 +0200

    Added development status 5 production/stable to classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     long_description=(open('README.rst').read() + "\n" +
                       open('CHANGES.rst').read()),
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Zope :: 3",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
